807265f96d6d798d9d0f64224e0550a4c3844de5
Steven Marquez <stevenmarquez1999@gmail.com>
2020-06-22 16:45:08 +0000

    Added coments, fixed styling, and added new tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -28,48 +28,50 @@ public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     if(request.getDuration() > TimeRange.WHOLE_DAY.duration()) return Arrays.asList();
     if(events.equals(Collections.emptySet())) return Arrays.asList(TimeRange.WHOLE_DAY);
-    else {
-      List<TimeRange> freeTime = new ArrayList<>();
-      for(Event event : events){
-        TimeRange beforeEvent = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, event.getWhen().start(), false);
-        TimeRange afterEvent = TimeRange.fromStartEnd(event.getWhen().end(), TimeRange.END_OF_DAY, true);
-        Set<String> modifiedAttendees = new HashSet<>(request.getAttendees());
-        modifiedAttendees.retainAll(event.getAttendees());
-        if(modifiedAttendees.size() >= 1){
-          freeTime.add(beforeEvent);
-          freeTime.add(afterEvent);
-        } else {
-          TimeRange allDay = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true);
-          freeTime.add(allDay);
+    //create List freeTime to hold the free time before and after an event
+    List<TimeRange> freeTime = new ArrayList<>();
+    for(Event event : events){
+      TimeRange beforeEvent = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, event.getWhen().start(), false);
+      TimeRange afterEvent = TimeRange.fromStartEnd(event.getWhen().end(), TimeRange.END_OF_DAY, true);
+      Set<String> modifiedAttendees = new HashSet<>(request.getAttendees());
+      modifiedAttendees.retainAll(event.getAttendees());
+      Set<String> optionalAttendees = new HashSet<>(request.getOptionalAttendees());
+      if(modifiedAttendees.size() >= 1){
+        freeTime.add(beforeEvent);
+        freeTime.add(afterEvent);
+      } else {
+        TimeRange allDay = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true);
+        freeTime.add(allDay);
+      }
+    }
+    //create List of updated options that take into account different circumstances
+    //ex: overlapping events
+    List<TimeRange> updatedOptions = new ArrayList<>();
+    for(int i = 0; i < freeTime.size(); i++){
+      TimeRange firstTime = freeTime.get(i);
+      for(int j = i+1; j < freeTime.size(); j++){
+        TimeRange secondTime = freeTime.get(j);
+        if(firstTime.overlaps(secondTime)){
+          int startTime = Math.max(firstTime.start(), secondTime.start());
+          int endTime = Math.min(firstTime.end(), secondTime.end());
+          TimeRange newOption = TimeRange.fromStartEnd(startTime, endTime, false);
+          if(!updatedOptions.contains(newOption) && startTime != endTime) updatedOptions.add(newOption);
         }
       }
-      List<TimeRange> updatedOptions = new ArrayList<>();
+    }
+    //if there is anything in updatedOptions, return those options
+    if(updatedOptions.size() == 0) {
       for(int i = 0; i < freeTime.size(); i++){
-        TimeRange firstTime = freeTime.get(i);
-        for(int j = i+1; j < freeTime.size(); j++){
-          TimeRange secondTime = freeTime.get(j);
-          if(firstTime.overlaps(secondTime)){
-            int startTime = Math.max(firstTime.start(), secondTime.start());
-            int endTime = Math.min(firstTime.end(), secondTime.end());
-            TimeRange newOption = TimeRange.fromStartEnd(startTime, endTime, false);
-            if(!updatedOptions.contains(newOption) && startTime != endTime) updatedOptions.add(newOption);
-          }
-        }
-      }
-      if(updatedOptions.size() == 0) {
-        for(int i = 0; i < freeTime.size(); i++){
-          TimeRange time = freeTime.get(i);
-          if(time.duration() < request.getDuration()) freeTime.remove(time);
-        }    
-        return freeTime;
-      }
-      else {
-        for(int i = 0; i < updatedOptions.size(); i++){
-          TimeRange time = updatedOptions.get(i);
-          if(time.duration() < request.getDuration()) updatedOptions.remove(time);
-        } 
-        return updatedOptions;
-        }
+        TimeRange time = freeTime.get(i);
+        if(time.duration() < request.getDuration()) freeTime.remove(time);
+      }    
+      return freeTime;
+    } else {
+      for(int i = 0; i < updatedOptions.size(); i++){
+        TimeRange time = updatedOptions.get(i);
+        if(time.duration() < request.getDuration()) updatedOptions.remove(time);
+      } 
+      return updatedOptions;
     }
   }
 }
